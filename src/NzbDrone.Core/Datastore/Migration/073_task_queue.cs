9f35e1b48b7181f7edd9508d55478a88ad327b8f
Mark McDowall <markus.mcd5@gmail.com>
2014-12-20 09:13:37 -0800

    migration started

---
+++ src/NzbDrone.Core/Datastore/Migration/073_task_queue.cs
@@ -0,0 +1,17 @@
+﻿using FluentMigrator;
+using NzbDrone.Core.Datastore.Migration.Framework;
+
+namespace NzbDrone.Core.Datastore.Migration
+{
+    [Migration(73)]
+    public class task_queue : NzbDroneMigrationBase
+    {
+        protected override void MainDbUpgrade()
+        {
+            Create.TableForModel("TaskQueue")
+                  .WithColumn("TypeName").AsString().NotNullable()
+                  .WithColumn("Priority").AsInt32().NotNullable()
+                  .WithColumn("Parameters").AsString().NotNullable();
+        }
+    }
+}
